bd93c1fae159932b94c1f6927e4be124975ea850
Pravin Mane <manepravin70@gmail.com>
2023-11-30 17:36:32 +0530

    updated file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="DimondPricePrediction",
+    version="0.0.1",
+    auther="Pravin Mane",
+    auther_email="manepravin70@gmail.com",
+    packages=find_packages()
+
+)
+  
