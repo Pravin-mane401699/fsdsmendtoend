3a58534004f5ab4f059412703de6314ce38e2305
Pravin Mane <manepravin70@gmail.com>
2023-12-11 15:46:32 +0530

    data ingestion update

--- src/DimondPricePrediction/components/data_ingestion.py
+++ src/DimondPricePrediction/components/data_ingestion.py
@@ -1,8 +1,47 @@
 
+import pandas as pd
+import numpy as np
+from src.DimondPricePrediction.logger import logging
+from src.DimondPricePrediction.exception import customexception
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+from pathlib import Path
+import os
+import sys
+
+class DataIngestionconfig:
+    raw_data_path:str=os.path.join("artifacts","raw.csv")
+    train_data_path:str=os.path.join("artifacts","train.csv")
+    test_data_path:str=os.path.join("artifacts","test.csv")
 
 class DataIngestion:
     def __init__(self):
-        pass
+        self.ingestion_config=DataIngestionconfig()
 
     def initiate_data_ingestion(self):
-        pass
+        logging.info("Data Ingestion started")
+
+        try:
+            data=pd.read_csv(Path(os.path.join("notebooks/data","gemstone.csv")))
+            logging.info("I have read the data frame as a data set")
+
+            os.makedirs(os.path.dirname(os.path.join(self.ingestion_config.raw_data_path)),exist_ok=True)
+            data.to_csv(self.ingestion_config.raw_data_path,index=False)
+            logging.info("I am saved the raw dataset in artifacts folder")
+
+            logging.info("here i have performed train test split")
+
+            train_data,test_data=train_test_split(data,test_size=0.25)
+            logging.info("train test split completed")
+
+            train_data.to_csv(self.ingestion_config.train_data_path,index=False)
+            test_data.to_csv(self.ingestion_config.test_data_path,index=False)
+            logging.info("Data ingestion part completed")
+
+
+
+        except Exception as e:
+            logging.info("exception during occured at data ingestion stage")
+            raise customexception(e,sys)
+
+
