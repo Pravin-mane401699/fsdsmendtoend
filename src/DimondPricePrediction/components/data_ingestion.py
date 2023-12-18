594ea45c507d58ee6142ab575295e968d8207fe0
Pravin Mane <manepravin70@gmail.com>
2023-12-18 19:58:11 +0530

    code updated

--- src/DimondPricePrediction/components/data_ingestion.py
+++ src/DimondPricePrediction/components/data_ingestion.py
@@ -39,9 +39,10 @@ class DataIngestion:
             logging.info("Data ingestion part completed")
 
 
-
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
         except Exception as e:
             logging.info("exception during occured at data ingestion stage")
             raise customexception(e,sys)
-
-
--- src/DimondPricePrediction/pipelines/training_pipeline.py
+++ src/DimondPricePrediction/pipelines/training_pipeline.py
@@ -1,5 +1,6 @@
 from src.DimondPricePrediction.components.data_ingestion import DataIngestion
-
+from src.DimondPricePrediction.components.data_transformation import DataTransfermation
+from src.DimondPricePrediction.components.model_trainer import ModelTrainer
 import os
 import sys
 from src.DimondPricePrediction.logger import logging
@@ -8,4 +9,11 @@ import pandas as pd
 
 obj=DataIngestion()
 
-obj.initiate_data_ingestion()
+train_data_path,test_data_path=obj.initiate_data_ingestion()
+
+data_transformation=DataTransfermation()
+
+train_arr,test_arr=data_transformation.initialize_data_transformation(train_data_path,test_data_path)
+
+Model_trainer_obj=ModelTrainer()
+Model_trainer_obj.initiate_model_training(train_arr,test_arr)
